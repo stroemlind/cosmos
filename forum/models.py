2a35cd69b2d34086c8785ba862331f91fe72c4a8
stroemlind <elin.stromlind@gmail.com>
2022-02-08 16:15:52 +0000

    Create Post model for forum

--- forum/models.py
+++ forum/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
+from tinymce.models import HTMLField
 
 # Create your models here.
+STATUS = ((0, 'Draft'), (1, 'Published'))
+
+class Post(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='forum_posts'
+    )
+    image = CloudinaryField('image', default='placeholder')
+    updated_on = models.DateTimeField(auto_now=True)
+    content = HTMLField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    likes = models.ManyToManyField(
+        User, related_name='forum_post_like', blank=True)
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return self.title
+
+    def number_of_likes(self):
+        return self.likes.count()
