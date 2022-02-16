ba8a85de366fba1bcd23b9dd8c76660ea355e305
stroemlind <elin.stromlind@gmail.com>
2022-02-16 16:34:03 +0000

    Create new view function for post detail

--- forum/urls.py
+++ forum/urls.py
@@ -3,8 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
-    path('post/<int:pk>', views.PostDetailView.as_view(), name='post-detail'),
-    path('add_post/', views.AddPost.as_view(), name='add-post'),
+    #path('post/<int:pk>', views.PostDetailView.as_view(), name='post-detail'),
+    path('post/<int:pk>', views.get_post, name='post-detail'),
+    # path('add_post/', views.AddPost.as_view(), name='add-post'),
+    path('add_post/', views.add_post, name='add-post'),
     path('post/edit/<int:pk>', views.EditPost.as_view(), name='edit-post'),
     path('post/<int:pk>delete/', views.DeletePost.as_view(), name='delete-post'),
 ]
--- forum/views.py
+++ forum/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404, reverse
+from django.shortcuts import render, get_object_or_404, reverse, redirect
 from django.views import generic, View
 from django.http import HttpResponseRedirect
 from django.urls import reverse_lazy
@@ -15,21 +15,48 @@ class PostList(generic.ListView):
     template_name = 'index.html'
 
 
-class PostDetailView(generic.DetailView):
-    """
-    The class to display the post in detail
-    """
-    model = Post
-    template_name = 'post_detail.html'
+#class PostDetailView(generic.DetailView):
+    #model = Post
+    #template_name = 'post_detail.html'
 
 
-class AddPost(generic.CreateView):
+def get_post(request, pk):
     """
-    The class to display text editor for users to create post
+    Function to se a post in detail
     """
-    model = Post
-    form_class = PostForm
-    template_name = 'add_post.html'
+    post = get_object_or_404(Post, pk=pk)
+    template = 'post_detail.html'
+    context = {
+        'post': post
+    }
+    return render(request, template, context)
+
+# class AddPost(generic.CreateView):
+    # """
+    # The class to display text editor for users to create post
+    # """
+    # model = Post
+    # form_class = PostForm
+    # template_name = 'add_post.html'
+
+
+def add_post(request):
+    """
+    Function to add a post to the forum
+    """
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES)
+        # context['posted'] = form.instance
+        if form.is_valid():
+            post = form.save()
+            post_id = post.pk
+            return redirect(get_post, post_id)
+    else:
+        form = PostForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'add_post.html', context)
 
 
 class EditPost(generic.UpdateView):
