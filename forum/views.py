d5d1971753ef56bde323fa29b5d081933a3d9a0f
stroemlind <elin.stromlind@gmail.com>
2022-02-18 10:03:06 +0000

    Add and set view for pagination to index.html

--- forum/views.py
+++ forum/views.py
@@ -13,6 +13,7 @@ class PostList(generic.ListView):
     model = Post
     queryset = Post.objects.filter(status=1).order_by('-created_on')
     template_name = 'index.html'
+    paginate_by = 12
 
 
 #class PostDetailView(generic.DetailView):
