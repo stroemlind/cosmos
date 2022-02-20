18d22f5a605703f18323b21937206515fc81e7f0
stroemlind <elin.stromlind@gmail.com>
2022-02-20 16:09:09 +0000

    Restrict users to not be able to change autor when adding blog post

--- forum/forms.py
+++ forum/forms.py
@@ -18,13 +18,32 @@ class PostForm(forms.ModelForm):
         The Meta class with what will be displayed and widgets
         """
         model = Post
-        fields = ('title', 'slug', 'author', 'image', 'category', 'content', 'status')
+        fields = (
+            'title',
+            'slug',
+            'author',
+            'image',
+            'category',
+            'content',
+            'status'
+            )
 
         widgets = {
             'title': forms.TextInput(attrs={'class': 'form-control'}),
-            'slug': forms.TextInput(attrs={'class': 'form-control'}),
-            'author': forms.Select(attrs={'class': 'form-control'}),
+            'slug': forms.TextInput(attrs={
+                'class': 'form-control',
+                'type': 'hidden'}
+                ),
+            'author': forms.TextInput(attrs={
+                'class': 'form-control',
+                'value': '',
+                'id': 'author-id',
+                'type': 'hidden'
+                }),
+            # 'author': forms.Select(attrs={'class': 'form-control'}),
             'image': forms.FileInput(attrs={'class': 'form-control'}),
-            'category': forms.Select(choices=choice_list, attrs={'class': 'form-control'}),
+            'category': forms.Select(choices=choice_list, attrs={
+                'class': 'form-control'}
+                ),
             'status': forms.Select(attrs={'class': 'form-control'})
         }
