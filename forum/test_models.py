c7b53a4b850edd79cacca7c39a3018be8b02d07b
stroemlind <elin.stromlind@gmail.com>
2022-03-02 10:52:20 +0000

    Remove out comment code from test

--- forum/test_models.py
+++ forum/test_models.py
@@ -67,7 +67,6 @@ class TestCommentModel(TestCase):
             content='test text body',
             created_on='2022.02.27',
             status=1,
-            # likes=likes.set(True)
             )
 
         test_post.save()
